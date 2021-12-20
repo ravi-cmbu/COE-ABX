fe14b72ee12973202e8648b6e4dd4a0143f4fda3
ravi-cmbu <92421435+ravi-cmbu@users.noreply.github.com>
2021-12-20 11:09:12 +0530

    0.2 version
    Action Version: 0.2

--- RK_ABX-3d68e6eb10b8022e07d1b874f0f68734f640e327ae2e9239413bb1d.py
+++ RK_ABX-3d68e6eb10b8022e07d1b874f0f68734f640e327ae2e9239413bb1d.py
@@ -1,5 +1,5 @@
 def handler(context, inputs):
-    greeting = "Hello, {0}!".format(inputs["target"])
+    greeting = "Hello, ravi {0}!".format(inputs["target"])
     print(greeting)
 
     outputs = {
