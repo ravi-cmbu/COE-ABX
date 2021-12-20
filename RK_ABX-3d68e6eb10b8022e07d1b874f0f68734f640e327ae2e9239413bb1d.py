67dfae33d3aeaab37217c5ef5d88983c8e87317d
ravi-cmbu <92421435+ravi-cmbu@users.noreply.github.com>
2021-12-20 15:40:27 +0530

    with custom properties modified
    Action Version: 0.4

--- RK_ABX-3d68e6eb10b8022e07d1b874f0f68734f640e327ae2e9239413bb1d.py
+++ RK_ABX-3d68e6eb10b8022e07d1b874f0f68734f640e327ae2e9239413bb1d.py
@@ -1,5 +1,11 @@
 def handler(context, inputs):
-    print("The parameter outputs are as follows")
-    outputs=inputs
-
+    print("Changing hostname from ABX")
+    
+    newName = "namedbyABX"
+    outputs={}
+    outputs["customProperties"] = inputs["customProperties"]
+    outputs["customProperties"]["name"] = newName
+    
+    print("The new name is : " + newName)
+    
     return outputs
